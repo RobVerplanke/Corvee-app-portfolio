037e7d24ae07e2bf78edcd170da4f776d0b9ab1a
Rob Verplanke <rverplanke@gmail.com>
2026-05-04 13:17:22 +0200

    chore: implement qrcode to app on the monitor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { getWeekNumber,
+import {
+  getWeekNumber,
   getNameOfDay,
   formatDate,
   getMostCommonMonth,
@@ -6,34 +7,35 @@ import { getWeekNumber,
   capitalizeFirstLetter,
   isValidName,
   getTodayForDatepicker,
-} from './helpers.js';
-import express from 'express';
-import session from 'express-session';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import Sequelize from 'sequelize';
-import Sqlite3 from '@vscode/sqlite3';
-import DatabaseHandler from './databaseHandler.js';
-import { request } from 'http';
-import { readFileSync } from 'fs';
+} from "./helpers.js";
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import Sequelize from "sequelize";
+import Sqlite3 from "@vscode/sqlite3";
+import DatabaseHandler from "./databaseHandler.js";
+import { request } from "http";
+import { readFileSync } from "fs";
+import QRCode from "qrcode";
 
 // Get month names from JSON locales file
-const t = JSON.parse(readFileSync('./locales/nl.json', 'utf-8'));
+const t = JSON.parse(readFileSync("./locales/nl.json", "utf-8"));
 
 // Truly constant values
 const MONTHS = t.common.months;
 const SCHEDULES_PER_MONTH = 4; // Amount of schedules displayed per month
 const DAYS_PER_WEEK = 5; // Working days
-const PASSWORD = '321';
+const PASSWORD = "321";
 
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
+  dialect: "sqlite",
   dialectModule: Sqlite3,
-  storage: 'database.sqlite',
+  storage: "database.sqlite",
   logging: false,
 });
 const databaseHandler = new DatabaseHandler(sequelize);
@@ -43,17 +45,19 @@ const databaseHandler = new DatabaseHandler(sequelize);
 await databaseHandler.sync();
 
 // Use static files for CSS-styling, scripts and assets (images)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Read form data (sent from dashboard) correctly
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 // Setup session middleware
-app.use(session({
-  secret: 'TEMP_SECRET_KEY',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "TEMP_SECRET_KEY",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 // Use local JSON-file as textual content storage
 app.use((req, res, next) => {
@@ -68,19 +72,19 @@ app.use((req, res, next) => {
 });
 
 // Set the view engine to ejs and build an absolute path to the views folder
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 function isLoggedIn(req, res, next) {
   if (req.session.isLoggedIn) {
     next();
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 }
 
 // Agenda page - Extra span-elements are used for the printed version
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   let requestedDate = null;
 
   // Create a RegExp to test the date for validity. If invalid, use today.
@@ -94,15 +98,22 @@ app.get('/', async (req, res) => {
   let weekNumbers = [];
   let currentWeekNumber = getWeekNumber(requestedDate); // Get current week number
   let year = requestedDate.getFullYear();
- 
+
   // Get a fixed amount of schedules for the upcoming weeks (defined in SCHEDULES_PER_MONTH constant) of the current month
-  for (let i=0; i<SCHEDULES_PER_MONTH; i++) {
-    schedules.push(await databaseHandler.getScheduleForWeek(year, currentWeekNumber+i));
-    weekNumbers.push(currentWeekNumber+i);
+  for (let i = 0; i < SCHEDULES_PER_MONTH; i++) {
+    schedules.push(
+      await databaseHandler.getScheduleForWeek(year, currentWeekNumber + i),
+    );
+    weekNumbers.push(currentWeekNumber + i);
   }
 
   // When the schedule is missing data for one or more days, add placeholder content for the missing days
-  let autoFilledSchedule = getAutoFilledSchedule(schedules, year, weekNumbers, DAYS_PER_WEEK)
+  let autoFilledSchedule = getAutoFilledSchedule(
+    schedules,
+    year,
+    weekNumbers,
+    DAYS_PER_WEEK,
+  );
 
   // Determine what is the most common month name in the schedules so it can be uses as page title
   let mostCommonMonth = MONTHS[getMostCommonMonth(schedules)];
@@ -113,7 +124,7 @@ app.get('/', async (req, res) => {
     getNameOfDay: getNameOfDay,
     formatDate: formatDate,
     capitalizeFirstLetter: capitalizeFirstLetter,
-  }
+  };
 
   // Data that needs to be displayd
   const data = {
@@ -121,23 +132,27 @@ app.get('/', async (req, res) => {
     weekNumbers: weekNumbers,
     mostCommonMonth: mostCommonMonth,
     requestedDate: req.query.date || getTodayForDatepicker(),
-  }
-  
+  };
+
   // activePage - function that highlights the corresponding navigation button of the active page
-  res.render('pages/agenda', { activePage: 'agenda', helper: helper, data: data });
+  res.render("pages/agenda", {
+    activePage: "agenda",
+    helper: helper,
+    data: data,
+  });
 });
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   // If already logged in, redirect to the dashboard for admin use.
   if (req.session.isLoggedIn) {
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
   } else {
     // If not logged in, show login and any error if present.
-    res.render('pages/login', { loginError: req.session.loginError });
+    res.render("pages/login", { loginError: req.session.loginError });
   }
-})
+});
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   // Retrieve the posted data for authentication. Only password for now.
   const password = req.body.password;
 
@@ -145,26 +160,26 @@ app.post('/login', (req, res) => {
   if (password == PASSWORD) {
     // Create login session and redirect to the admin view
     req.session.isLoggedIn = true;
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
   } else {
     // Reject login, go back to login page and show warning
     req.session.loginError = true;
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
 
 // Logout and return to agenda.
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.log(err);
     }
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
 // Admin dashboard page
-app.get('/dashboard', isLoggedIn, async (req, res) => {
+app.get("/dashboard", isLoggedIn, async (req, res) => {
   let requestedDate = null;
 
   // Create a RegExp to test the date for validity. If invalid, use today.
@@ -180,15 +195,22 @@ app.get('/dashboard', isLoggedIn, async (req, res) => {
   let currentWeekNumber = getWeekNumber(requestedDate); // Get current week number
   let year = requestedDate.getFullYear();
   let volunteers = await databaseHandler.getVolunteers(); // Get all volunteer names
-  
+
   // Get a fixed amount of schedules for the upcoming weeks (defined in SCHEDULES_PER_MONTH constant) of the current month
-  for (let i=0; i<SCHEDULES_PER_MONTH; i++) {
-    schedules.push(await databaseHandler.getScheduleForWeek(year, currentWeekNumber+i));
-    weekNumbers.push(currentWeekNumber+i); // Corresponding weeknumbers will be used as table titles
+  for (let i = 0; i < SCHEDULES_PER_MONTH; i++) {
+    schedules.push(
+      await databaseHandler.getScheduleForWeek(year, currentWeekNumber + i),
+    );
+    weekNumbers.push(currentWeekNumber + i); // Corresponding weeknumbers will be used as table titles
   }
 
   // When the schedule is missing data for one or more days, calculate dates for the missing days
-  let autoFilledSchedule = getAutoFilledSchedule(schedules, year, weekNumbers, DAYS_PER_WEEK);
+  let autoFilledSchedule = getAutoFilledSchedule(
+    schedules,
+    year,
+    weekNumbers,
+    DAYS_PER_WEEK,
+  );
 
   // Determine what is the most common month name in the schedules so it can be used as page title
   let mostCommonMonth = MONTHS[getMostCommonMonth(schedules)];
@@ -199,11 +221,11 @@ app.get('/dashboard', isLoggedIn, async (req, res) => {
     getNameOfDay: getNameOfDay,
     formatDate: formatDate,
     capitalizeFirstLetter: capitalizeFirstLetter,
-    toLocalDateString: (date) => { 
+    toLocalDateString: (date) => {
       // Use local time instead of UTC-time. This fixes shifting of time when a new schedule is added, which resulted in different days having the same date.
-      return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
-    }
-  }
+      return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+    },
+  };
 
   // Data that needs to be displayd
   const data = {
@@ -212,83 +234,103 @@ app.get('/dashboard', isLoggedIn, async (req, res) => {
     requestedDate: req.query.date || getTodayForDatepicker(),
     weekNumbers: weekNumbers,
     volunteers: volunteers,
-  }
+  };
 
   // Alert messages
   const alerts = {
     success: req.query.success,
     removed: req.query.removed,
     edited: req.query.edited,
-    error: req.query.error    ,
-    errorDouble: req.query.errorDouble
-  }
-  
+    error: req.query.error,
+    errorDouble: req.query.errorDouble,
+  };
+
   // activePage - function that highlights the corresponding navigation button of the active page
-  res.render('pages/dashboard', { activePage: 'dashboard', helper: helper, data: data, alerts: alerts});
+  res.render("pages/dashboard", {
+    activePage: "dashboard",
+    helper: helper,
+    data: data,
+    alerts: alerts,
+  });
 });
 
 // Admin dashboard page - Save new schedule from form
-app.post('/dashboard/save', isLoggedIn, async (req, res) => {
+app.post("/dashboard/save", isLoggedIn, async (req, res) => {
   let formData = req.body;
 
   for (const [data, dayParts] of Object.entries(formData)) {
-    await databaseHandler.updateScheduleEntry (new Date(data), dayParts).catch(async (error) => {
-      // On error create.
-      await databaseHandler.addScheduleEntry(new Date(data), dayParts);
-    });
+    await databaseHandler
+      .updateScheduleEntry(new Date(data), dayParts)
+      .catch(async (error) => {
+        // On error create.
+        await databaseHandler.addScheduleEntry(new Date(data), dayParts);
+      });
   }
-  res.redirect('/dashboard?edited=true');
+  res.redirect("/dashboard?edited=true");
 });
 
 // Admin dashboard page - Add new volunteer after name validation
-app.post('/dashboard/add', isLoggedIn, async (req, res) => {
+app.post("/dashboard/add", isLoggedIn, async (req, res) => {
   const formData = req.body;
   const newVolunteer = formData.newVolunteer;
 
-  if (!isValidName(newVolunteer)) { // Regex validation
-    res.redirect('/dashboard?error=true'); 
+  if (!isValidName(newVolunteer)) {
+    // Regex validation
+    res.redirect("/dashboard?error=true");
   } else {
     try {
       await databaseHandler.addVolunteer(newVolunteer);
-      res.redirect('/dashboard?success=true');
+      res.redirect("/dashboard?success=true");
     } catch (err) {
-      if (err.name === 'SequelizeUniqueConstraintError') {
-        res.redirect('/dashboard?errorDouble=true');
+      if (err.name === "SequelizeUniqueConstraintError") {
+        res.redirect("/dashboard?errorDouble=true");
       } else {
-        res.redirect('/dashboard?error=true');
+        res.redirect("/dashboard?error=true");
       }
     }
   }
 });
 
 // Admin dashboard page - Remove existing volunteer
-app.post('/dashboard/delete', isLoggedIn, (req, res) => {
+app.post("/dashboard/delete", isLoggedIn, (req, res) => {
   const formData = req.body;
   const volunteerId = formData.volunteerId;
 
   databaseHandler.removeVolunteer(volunteerId);
-  res.redirect('/dashboard?removed=true');
+  res.redirect("/dashboard?removed=true");
 });
 
 // Instructions manual page
-app.get('/manuals', (req, res) => {
-  res.render('pages/manuals', { activePage: 'manuals', activeManual: 'tasks'});
+app.get("/manuals", (req, res) => {
+  res.render("pages/manuals", { activePage: "manuals", activeManual: "tasks" });
 });
 
-app.get('/manuals/tasks', (req, res) => {
-  res.render('pages/manuals', { activePage: 'manuals', activeManual: 'tasks'});
+app.get("/manuals/tasks", (req, res) => {
+  res.render("pages/manuals", { activePage: "manuals", activeManual: "tasks" });
 });
 
-app.get('/manuals/dishwasher', (req, res) => {
-  res.render('pages/manuals', { activePage: 'manuals', activeManual: 'dishwasher'});
+app.get("/manuals/dishwasher", (req, res) => {
+  res.render("pages/manuals", {
+    activePage: "manuals",
+    activeManual: "dishwasher",
+  });
 });
 
-app.get('/manuals/cleaning', (req, res) => {
-  res.render('pages/manuals', { activePage: 'manuals', activeManual: 'cleaning'});
+app.get("/manuals/cleaning", (req, res) => {
+  res.render("pages/manuals", {
+    activePage: "manuals",
+    activeManual: "cleaning",
+  });
 });
 
 // A clean agenda view to display on an external monitor
-app.get('/monitor', async (req, res) => {
+app.get("/monitor", async (req, res) => {
+  const qrCode = await QRCode.toDataURL("http://corvee.aletho.nl", {
+    color: {
+      dark: "#1C3269",
+      light: "#00000000",
+    },
+  });
   let requestedDate = null;
 
   // Create a RegExp to test the date for validity. If invalid, use today.
@@ -302,15 +344,22 @@ app.get('/monitor', async (req, res) => {
   let weekNumbers = [];
   let currentWeekNumber = getWeekNumber(requestedDate); // Get current week number
   let year = requestedDate.getFullYear();
- 
+
   // Get a fixed amount of schedules for the upcoming weeks (defined in SCHEDULES_PER_MONTH constant) of the current month
-  for (let i=0; i<SCHEDULES_PER_MONTH; i++) {
-    schedules.push(await databaseHandler.getScheduleForWeek(year, currentWeekNumber+i));
-    weekNumbers.push(currentWeekNumber+i);
+  for (let i = 0; i < SCHEDULES_PER_MONTH; i++) {
+    schedules.push(
+      await databaseHandler.getScheduleForWeek(year, currentWeekNumber + i),
+    );
+    weekNumbers.push(currentWeekNumber + i);
   }
 
   // When the schedule is missing data for one or more days, add placeholder content for the missing days
-  let autoFilledSchedule = getAutoFilledSchedule(schedules, year, weekNumbers, DAYS_PER_WEEK)
+  let autoFilledSchedule = getAutoFilledSchedule(
+    schedules,
+    year,
+    weekNumbers,
+    DAYS_PER_WEEK,
+  );
 
   // Determine what is the most common month name in the schedules so it can be uses as page title
   let mostCommonMonth = MONTHS[getMostCommonMonth(schedules)];
@@ -321,7 +370,7 @@ app.get('/monitor', async (req, res) => {
     getNameOfDay: getNameOfDay,
     formatDate: formatDate,
     capitalizeFirstLetter: capitalizeFirstLetter,
-  }
+  };
 
   // Data that needs to be displayd
   const data = {
@@ -329,17 +378,22 @@ app.get('/monitor', async (req, res) => {
     weekNumbers: weekNumbers,
     mostCommonMonth: mostCommonMonth,
     requestedDate: req.query.date || getTodayForDatepicker(),
-  }
-  
+    qrCode,
+  };
+
   // activePage - function that highlights the corresponding navigation button of the active page
-  res.render('pages/monitor', { activePage: 'agenda', helper: helper, data: data });
+  res.render("pages/monitor", {
+    activePage: "agenda",
+    helper: helper,
+    data: data,
+  });
 });
 
 // Eror page
-app.use('/{*splat}', (req, res) => {
-  res.render('pages/errorpage');
+app.use("/{*splat}", (req, res) => {
+  res.render("pages/errorpage");
 });
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
