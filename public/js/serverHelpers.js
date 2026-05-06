4aeaf1600bb8ca106d9317b0021d82dc732fa253
Rob Verplanke <rverplanke@gmail.com>
2026-05-06 11:50:04 +0200

    chore: add day-off option to dashboard selections
    Implement option groups to categorize names from 'special' options

--- public/js/serverHelpers.js
+++ public/js/serverHelpers.js
@@ -2,17 +2,19 @@
 let url = new URL(window.location.href);
 
 // And add event listeners on toolbar dropdown buttons
-document.addEventListener('click', (e) => {
-  if (!e.target.closest('.toolbar')) {
-    document.querySelectorAll('.toolbar-dropdown').forEach(d => d.classList.remove('open'));
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".toolbar")) {
+    document
+      .querySelectorAll(".toolbar-dropdown")
+      .forEach((d) => d.classList.remove("open"));
   }
 });
 
 // Print agenda page from dashboard page
 function printPage() {
-  const frame = document.getElementById('print-frame');
-  frame.src = '/';
-  frame.onload = function() {
+  const frame = document.getElementById("print-frame");
+  frame.src = "/";
+  frame.onload = function () {
     frame.contentWindow.print();
   };
 }
@@ -21,31 +23,46 @@ function printPage() {
 function getToday() {
   const today = new Date();
   const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
 
 // Handle unsaved changes when the agenda is adjusted
 function onDateChanged(e) {
   // Ignore same date.
-  if (url.searchParams.has('date') && url.searchParams.get('date') == e.target.value) {
+  if (
+    url.searchParams.has("date") &&
+    url.searchParams.get("date") == e.target.value
+  ) {
     return;
   }
   // Warn if a volunteer was changed if it's the dashboard page.
-  if (url.pathname.includes('dashboard') && window.volunteerChanged) {
+  if (url.pathname.includes("dashboard") && window.volunteerChanged) {
     // If the confirm is rejected, ignore changing url and reset datepicker. Else proceed to change the page.
     if (!confirm(window.appTexts.warning)) {
-      e.target.value = url.searchParams.has('date') ? url.searchParams.get('date') : getToday();
+      e.target.value = url.searchParams.has("date")
+        ? url.searchParams.get("date")
+        : getToday();
       return;
     }
   }
   // It should be safe to change the date.
-  url.searchParams.set('date', e.target.value);
+  url.searchParams.set("date", e.target.value);
   window.location.href = url;
 }
 
+// Style the "day off"-option in selections on the dashboard page
+function setClassListDayOff(select) {
+  if (select.value === "dayOff") {
+    select.classList.add("select-closed");
+  } else {
+    select.classList.remove("select-closed");
+  }
+}
+
 // Set a flag when a volunteer has been changed to a value different from its initial value.
-function onVolunteerChanged(e) {
+function onVolunteerChanged(select) {
   window.volunteerChanged = true;
+  setClassListDayOff(select); // Style "day off"-option while iterating through the options
 }
